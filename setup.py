05bc7347b917e6d0e3ff00e63639ed0db18f73c9
luozhouyang <zhouyang.luo@gmail.com>
2020-06-10 11:27:44 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="naivenlp",
+    version="0.0.1",
+    description="NLP toolkit, including tokenization, sequence tagging, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luozhouyang/naivenlp",
+    author="ZhouYang Luo",
+    author_email="zhouyang.luo@gmail.com",
+    packages=setuptools.find_packages(),
+    # include_package_data=True,
+    package_data={
+        "naivenlp": [
+            "tokenizers/data/dict.txt",
+            "tokenizers/data/prob_emit.p",
+            "tokenizers/data/prob_start.p",
+            "tokenizers/data/prob_trans.p",
+        ]
+    },
+    install_requires=[
+
+    ],
+    extras_require={
+
+    },
+    license="Apache Software License",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    )
+)
