4c0f9fe72c353805ee3646271ba3f5992f28e599
luozhouyang <zhouyang.luo@gmail.com>
2020-06-10 11:39:27 +0800

    Add version

--- naivenlp/__init__.py
+++ naivenlp/__init__.py
@@ -4,6 +4,10 @@ from .tokenizers.abstract_tokenizer import (AbstractTokenizer,
                                             VocabBasedTokenizer)
 from .tokenizers.jieba_tokenizer import JiebaTokenizer
 
+__version__ = "0.0.1"
+__name__ = "naivenlp"
+
+
 logging.basicConfig(
     format="%(asctime)s %(levelname)7s %(filename)15s %(lineno)4d] %(message)s",
     level=logging.INFO
