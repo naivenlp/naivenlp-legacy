aae3cef04f5057a0ad9ba4609745047eb5393bb1
luozhouyang <zhouyang.luo@gmail.com>
2020-07-02 19:29:36 +0800

    Add transformer tokenizer

--- naivenlp/tokenizers/__init__.py
+++ naivenlp/tokenizers/__init__.py
@@ -1,4 +1,4 @@
 from .abstract_tokenizer import AbstractTokenizer, CustomTokenizer
 from .bert_tokenizer import BertTokenizer
 from .jieba_tokenizer import JiebaTokenizer
-from .language_model_tokenizer import LanguageModelTokenizer
+from .language_model_tokenizer import LanguageModelTokenizer, TransformerTokenizer
