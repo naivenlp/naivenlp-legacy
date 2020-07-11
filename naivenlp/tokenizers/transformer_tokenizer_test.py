64c45d30f82c5d566e647ab45922f0c7da1e3c29
luozhouyang <zhouyang.luo@gmail.com>
2020-07-11 20:12:46 +0800

    Append mismatch special tokens to vocab's tail

--- naivenlp/tokenizers/transformer_tokenizer_test.py
+++ naivenlp/tokenizers/transformer_tokenizer_test.py
@@ -15,6 +15,12 @@ class TransformerTokenizerTest(unittest.TestCase):
         self.assertEqual(104, tokenizer.bos_id)
         self.assertEqual(105, tokenizer.eos_id)
 
+        tokenizer = TransformerTokenizer(vocab_file='testdata/vocab_chinese.txt', bos_token='S', eos_token='</S>')
+        self.assertEqual(0, tokenizer.pad_id)
+        self.assertEqual(100, tokenizer.unk_id)
+        self.assertEqual(21127, tokenizer.bos_id)
+        self.assertEqual(21128, tokenizer.eos_id)
+
 
 if __name__ == "__main__":
     unittest.main()
